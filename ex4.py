f51954850ec9a9278919e4f3b0025763719d19d3
Hamedalabri <148535721+Hamedalabri@users.noreply.github.com>
2023-11-17 16:48:42 +0100

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,19 @@
+factorielle = 1
+n = int(input("Valeur de n: "))
+for i in range(1,n+1):
+    factorielle = factorielle *i
+
+
+print("la factorielle est: ", factorielle)
+"""
+factorielle = 1
+i= 1
+n = int(input("Valeur de n: "))
+while i!= n+1:
+
+    factorielle = factorielle* i
+    i = i+1
+
+
+
+print("la factorielle est: ", factorielle)"""
